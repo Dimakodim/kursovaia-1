9a3e7f9cf489cbd4d0b652810bc3383a5e18c0bc
Dimako <dimako920@gmail.com>
2026-02-25 16:21:31 +0700

    feat(docs): оформлен отчет для первого спринта

---
+++ to_graph.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as mb
+
+f = open("data_sprint.txt").readlines()
+
+data = [(int(x.split(" ")[0]), int(x.split(" ")[1])) for x in f]
+
+data_y = []
+data_x = []
+
+for i in data:
+    for y in range(len(data_y)):
+        data_y[y] += i[0]
+    data_y.append(i[0])
+    data_y.append(i[0])
+    for x in range(len(data_x)):
+        data_x[x] += i[1]
+    data_x.append(i[1] + 0.001)
+    data_x.append(i[1])
+
+data_y.append(0)
+data_x.append(0)
+data_x.reverse()
+
+print(data_y)
+print(data_x)
+
+mb.plot(data_x, data_y)
+mb.xlabel("Время, дни")
+mb.ylabel("Вес, сторипоинты")
+mb.show()
