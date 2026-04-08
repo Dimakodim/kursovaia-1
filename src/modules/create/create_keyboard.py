ac9838b9fdf00397bf2f2083b5befc06dfd1dc14
Dimako <dimako920@gmail.com>
2026-04-08 12:56:42 +0700

    feat(bot): добавлен модуль create

---
+++ src/modules/create/create_keyboard.py
@@ -0,0 +1,6 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+def create_kb():
+    kb_list = [[KeyboardButton(text="Отмена ❌")]]
+    keyboard = ReplyKeyboardMarkup(keyboard=kb_list, resize_keyboard=True, one_time_keyboard=True)
+    return keyboard
--- src/modules/start/start_module.py
+++ src/modules/start/start_module.py
@@ -1,14 +1,13 @@
 import aiogram
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, StateFilter
 from aiogram.types import Message
 from .start_keyboard import *
 from .start_db import *
 
 start_router = Router()
 
-@start_router.message(CommandStart())
+@start_router.message(CommandStart(), StateFilter(None))
 async def command_start_handler(message: Message) -> None:
+    addUser(message.from_user.id)
     await message.answer(f'Привет, {message.from_user.first_name}!\nЯ бот для очередей!', reply_markup=start_kb())
-
-
