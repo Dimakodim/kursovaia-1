84e21ef8a522ae47a5d58ccbe532da16cbfdc2a2
Dimako <dimako920@gmail.com>
2026-05-06 14:40:14 +0700

    feat(bot): добавлена инлайн клавиатура дял смены позиции в очереди

--- src/modules/show_queue/show_queue_keyboard.py
+++ src/modules/show_queue/show_queue_keyboard.py
@@ -1,6 +1,9 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 
 def show_queue_kb():
-    kb_list = [[KeyboardButton(text="show_queue")]]
-    keyboard = ReplyKeyboardMarkup(keyboard=kb_list, resize_keyboard=True, one_time_keyboard=True)
+    kb_list = [
+    [InlineKeyboardButton(text="Я сдал", callback_data="btn_1")],
+    [InlineKeyboardButton(text="В конец очереди", callback_data="btn_2"), InlineKeyboardButton(text="Пропустить одного", callback_data="btn_3")],
+    [InlineKeyboardButton(text="Покинуть очередь", callback_data="btn_4")]]
+    keyboard = InlineKeyboardMarkup(inline_keyboard=kb_list)
     return keyboard
--- src/modules/show_queue/show_queue_module.py
+++ src/modules/show_queue/show_queue_module.py
@@ -8,6 +8,7 @@ from .show_queue_keyboard import *
 from .show_queue_db import *
 from ..list.list_module import ListState
 from aiogram.utils.formatting import Text, BlockQuote, Code, TextLink
+from ..start import start_keyboard
 import json
 
 show_queue_router = Router()
@@ -17,22 +18,25 @@ async def id_queue_handler(message: Message, state: FSMContext, bot: Bot) -> Non
     try:
         queue = showQueue(message.from_user.id, message.text)
         if len(queue) == 0:
-            await message.answer("Ошибка! Такой очереди не существует!", reply_markup=None)
+            await message.answer("Ошибка! Такой очереди не существует!")
         else:
             list_queue = json.loads(queue[0][4])
             text_queue = []
             for i in range(len(list_queue)):
+                you_text = ''
                 user_id = getTgIdById(list_queue[i])
                 if user_id:
+                    if user_id[0] == message.from_user.id: you_text = ' - вы'
                     chat = await bot.get_chat(chat_id=user_id[0])
-                    text_queue.append(BlockQuote(f'{i+1}) {chat.full_name}'))
-
+                    text_queue.append(BlockQuote(f'{i+1}) {chat.full_name}'+you_text))
             admin_id = getTgIdById(queue[0][2])
             admin_text = ''
             if admin_id:
                 chat = await bot.get_chat(chat_id=admin_id[0])
                 admin_text = ['\n\nАдминистратор очереди: ', Code(chat.full_name)]
             content = Text(Code(queue[0][1]), '\n\n', *text_queue, *admin_text)
-            await message.answer(**content.as_kwargs(), reply_markup=None)
+            await message.answer(**content.as_kwargs(), reply_markup=show_queue_kb())
+            await state.clear()
+
     except ValueError:
         await message.answer("Введите корректный айди очереди")
